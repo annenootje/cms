49c0535af802745b35c0c402d0e20132d8be1acc
Anne Noteboom <annenootje@live.nl>
2020-03-26 14:15:44 +0100

    Additional files

--- public/js/app.js
+++ public/js/app.js
@@ -34268,8 +34268,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/anne/Documents/CMS/laravel/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/anne/Documents/CMS/laravel/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/anne/Documents/GitHub/cms/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/anne/Documents/GitHub/cms/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
